72996999974510847a4a5f27827d3e7361f556e5
sinanovicanes <sinanovicanes01@gmail.com>
2024-07-29 23:01:25 +0300

    Fix: update user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/User';
 import { Model } from 'mongoose';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -14,12 +15,17 @@ export class UserService {
     }
 
     const id = target instanceof User ? target._id : target;
+
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
+    }
+
     const results = await this.userModel.findByIdAndUpdate(id, updateUserDto);
 
     if (!results) {
       throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
 
-    return results;
+    return { message: 'User updated successfully' };
   }
 }
