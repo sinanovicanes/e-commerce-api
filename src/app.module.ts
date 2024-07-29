348f35e27e1cce6bab1d891643076dd02a8d7df2
sinanovicanes <sinanovicanes01@gmail.com>
2024-07-29 15:45:30 +0300

    MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './config/env.validation';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: 'ecommerce',
     }),
   ],
   controllers: [AppController],
