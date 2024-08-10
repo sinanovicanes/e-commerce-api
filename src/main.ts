43ccd0735f7285f904ff2dfec55ff4eb01ea2d84
sinanovicanes <sinanovicanes01@gmail.com>
2024-08-11 02:54:47 +0300

    Set global prefix for API endpoints

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
