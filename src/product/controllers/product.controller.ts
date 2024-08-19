fa74491cfb84c686871aa8086631544c34aeac8f
sinanovicanes <sinanovicanes01@gmail.com>
2024-08-20 02:33:35 +0300

    chore: Update Swagger documentation for ProductController API endpoints

--- src/product/controllers/product.controller.ts
+++ src/product/controllers/product.controller.ts
@@ -17,17 +17,37 @@ import { Types } from 'mongoose';
 import { CreateProductDto, UpdateProductDto } from '../dtos';
 import { ProductService } from '../services';
 import { ProductGuard, ProductMerchantAccessGuard } from '../guards';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiHeader,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { Product } from '../schemas';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @ApiOkResponse({ description: 'Get product details', type: Product })
+  @ApiNotFoundResponse({ description: 'Product not found' })
   @Public()
   @Get('/:productId')
   getProduct(@Param('productId', ParseObjectIdPipe) productId: Types.ObjectId) {
     return this.productService.findPublicProductById(productId, true);
   }
 
+  @ApiHeader({ name: 'x-merchant-id', description: 'Merchant ID' })
+  @ApiCreatedResponse({
+    description: 'Product created successfully',
+    type: Product,
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   @UseGuards(MerchantAccessGuard)
   @Post()
   createProduct(
@@ -37,6 +57,11 @@ export class ProductController {
     return this.productService.createProduct(merchant, createProductDto);
   }
 
+  @ApiHeader({ name: 'x-merchant-id', description: 'Merchant ID' })
+  @ApiOkResponse({ description: 'Product updated successfully', type: Product })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Product not found' })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   @UseGuards(MerchantAccessGuard, ProductGuard, ProductMerchantAccessGuard)
   @Patch('/:productId')
   updateProduct(
@@ -46,13 +71,16 @@ export class ProductController {
     return this.productService.updateProduct(productId, updateProductDto);
   }
 
+  @ApiHeader({ name: 'x-merchant-id', description: 'Merchant ID' })
+  @ApiOkResponse({ description: 'Product deleted successfully', type: Product })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Product not found' })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   @UseGuards(MerchantAccessGuard, ProductGuard, ProductMerchantAccessGuard)
   @Delete('/:productId')
-  async deleteProduct(
+  deleteProduct(
     @Param('productId', ParseObjectIdPipe) productId: Types.ObjectId,
   ) {
-    const review = await this.productService.deleteProduct(productId);
-
-    return { message: 'Product deleted successfully', review };
+    return this.productService.deleteProduct(productId);
   }
 }
