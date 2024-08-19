9eb0ec84576026eeede21cb6ae6f9066e3e255c4
sinanovicanes <sinanovicanes01@gmail.com>
2024-08-20 02:48:47 +0300

    chore: Update user controller with Swagger decorators and responses

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -3,17 +3,34 @@ import { Body, Controller, Get, Patch } from '@nestjs/common';
 import { UpdateUserDto } from '../dtos';
 import { User } from '../schemas';
 import { UserService } from '../services';
+import {
+  ApiBadRequestResponse,
+  ApiCookieAuth,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('users')
+@ApiCookieAuth()
+@ApiUnauthorizedResponse({ description: 'Unauthorized' })
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @ApiOkResponse({ description: 'Get current user details', type: User })
   @Get()
   async getCurrentUser(@GetUser() user: User) {
     return user;
   }
 
+  @ApiOkResponse({
+    description: 'User details updated successfully',
+    type: User,
+  })
+  @ApiBadRequestResponse({ description: 'Invalid data' })
   @Patch()
-  async update(@GetUser() user: User, @Body() updateUserDto: UpdateUserDto) {
-    return await this.userService.updateUser(user, updateUserDto);
+  update(@GetUser() user: User, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateUser(user, updateUserDto);
   }
 }
